d3b10e386262631ecafb6a5067ccf018c140508a
aatropkin <40324229+aatropkin@users.noreply.github.com>
2022-07-14 09:55:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let array = [];
+    let arrayTwo = [];
+    let result = [];
+
+    for (let i = 0; i < expr.length; i += 10) {
+        array.push(expr.slice(i, i + 10) );
+    }
+
+    for (let j = 0; j < array.length; j ++) {
+        arrayTwo.push(array[j].replace(/10/gi, ".").replace(/11/gi, "-").replace(/0/gi, "").replace(/\*{10}/gi, " ") );
+    }
+
+    arrayTwo.forEach( (element) => {
+        if (MORSE_TABLE[element] ) {
+          result.push(MORSE_TABLE[element] );
+        } else {
+          result.push(" ");
+        }
+      })
+      return result.join("");
 }
 
 module.exports = {
